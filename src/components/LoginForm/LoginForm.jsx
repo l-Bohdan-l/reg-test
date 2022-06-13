f2c193ff7508c65aee5e5ad3b91e0e29f883367c
l-Bohdan-l <bpapushoj@gmail.com>
2022-06-13 18:39:50 +0300

    fix

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,25 +1,47 @@
+/* eslint-disable no-console */
+/* eslint-disable react/react-in-jsx-scope */
 import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+// import { useDispatch } from 'react-redux';
 
-export default function LoginForm(props) {
-    const [email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
-    const dispatch = useDispatch();
+import { Formik, Form, Field } from 'formik';
 
-    const handleChange = ({ target: { name, value } }) => { 
-        switch (name) { 
-            case 'email': 
-                return setEmail(value);
-            case 'password':
-                return setPassword(value);
-            default:
-                return;
-        }
+export default function LoginForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  //   const dispatch = useDispatch();
+
+  const initialValues = {
+    email: '',
+    password: '',
+  };
+
+  // eslint-disable-next-line consistent-return
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'email':
+        console.log(email);
+        return setEmail(value);
+      case 'password':
+        console.log(password);
+        return setPassword(value);
+      default:
     }
+  };
 
-    return (
-        <>
-            
-        </>
-    )
+  const handleSubmit = (values) => {
+    // eslint-disable-next-line no-console
+    console.log(values);
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+    >
+      <Form autoComplete="off">
+        <Field name="email" type="email" placeholder="Email" onChange={handleChange} />
+        <Field name="password" type="password" placeholder="Password" onChange={handleChange} />
+      </Form>
+    </Formik>
+  );
 }
--- src/redux/auth/auth-operations.js
+++ src/redux/auth/auth-operations.js
@@ -3,42 +3,42 @@ import {
   registerApi,
   logIn,
   logOut,
-  fetchCurrentUser,
-  token,
-} from '../../services/userApi';
+//   fetchCurrentUser,
+//   token,
+} from '../../services/userApi/userApi';
 
 export const registraion = createAsyncThunk(
-    'auth/registration',
-    async (credentials, {rejectWithValue}) => { 
-        try {
-            const response = await registerApi(credentials);
-            return response;
-        } catch (error) {
-            return rejectWithValue(error.message); 
-        }
+  'auth/registration',
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const response = await registerApi(credentials);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
-)
+  },
+);
 
 export const login = createAsyncThunk(
-    'auth/login',
-    async (credentials, { rejectWithValue }) => { 
-        try {
-            const response = await logIn(credentials);
-            return response;
-        } catch (error) {
-            return rejectWithValue(error.message); 
-        }
+  'auth/login',
+  async (credentials, { rejectWithValue }) => {
+    try {
+      const response = await logIn(credentials);
+      return response;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
-)
+  },
+);
 
 export const logout = createAsyncThunk(
-    'auth/logout',
-    async (_, { rejectWithValue }) => { 
-        try {
-            await logOut();
-            return;
-        } catch (error) {
-            return rejectWithValue(error.message); 
-        }
+  'auth/logout',
+  async (_, { rejectWithValue }) => {
+    try {
+      await logOut();
+      return;
+    } catch (error) {
+      return rejectWithValue(error.message);
     }
-)
+  },
+);
